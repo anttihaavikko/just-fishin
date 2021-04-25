0d922641f4843621c380401397fcd3d51534ff67
Antti Haavikko <antti.haavikko@gmail.com>
2021-04-25 21:59:56 +0300

    Fix build pushing script path

--- pushbuilds.sh
+++ pushbuilds.sh
@@ -4,15 +4,15 @@ NC='\033[0m' # No Color
 echo " "
 
 echo "${COLOR}Pushing build for OSX${NC}"
-butler push Builds/osx anttihaavikko/tic-tac-matrix:osx --fix-permissions
+butler push Builds/osx anttihaavikko/just-fishin:osx --fix-permissions
 
 echo "${COLOR}Pushing build for Windows${NC}"
-butler push Builds/win anttihaavikko/tic-tac-matrix:win --fix-permissions
+butler push Builds/win anttihaavikko/just-fishin:win --fix-permissions
 
 echo "${COLOR}Pushing build for Linux${NC}"
-butler push Builds/linux anttihaavikko/tic-tac-matrix:linux
+butler push Builds/linux anttihaavikko/just-fishin:linux
 
 echo "${COLOR}Copying html5 files over to correct path"
 cp -a Builds/webgl/html5/Build/. Builds/html5/Build
 echo "${COLOR}Pushing build for HTML5${NC}"
-butler push Builds/html5 anttihaavikko/tic-tac-matrix:html5
+butler push Builds/html5 anttihaavikko/just-fishin:html5
